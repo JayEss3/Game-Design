843345825b23c41efedfadd08f54c04fe51df1bf
Bryan Portieles <ninjakiller231@gmail.com>
2020-02-13 21:24:54 -0500

    added to a file under my name, the script)

---
+++ GamersGaming/Assets/2. Bryan Portieles/Bullet_Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet_Script : MonoBehaviour
+{
+   [SerializeField] public float bulletSpeed;
+   [SerializeField] public float damage;
+
+   private void OnCollisionEnter(Collision collision)
+   {
+      if (collision.gameObject.tag != "Weapon")
+      {
+         collision.gameObject.SendMessage("ApplyDamage", damage);
+         Destroy(gameObject);
+      }
+   }
+   public void Fire()
+   {
+      var rb = GetComponent<Rigidbody>();
+      rb.velocity = transform.forward * bulletSpeed;
+      transform.parent = null;
+   }
+    
+
+    
+}
