973bdc0ecc65e87ef61f02cb481409ba825d305d
Calvin Williams <calvinjwilliams97@gmail.com>
2020-03-02 16:28:36 -0500

    Added rigidbody player movement

--- GamersGaming/Assets/Scripts/Player/PlayerController.cs
+++ GamersGaming/Assets/Scripts/Player/PlayerController.cs
@@ -4,26 +4,56 @@ using UnityEngine;
 using UnityEngine.UI;
 using Mirror;
 using TMPro;
+
 public class PlayerController : NetworkBehaviour
 {
-    [Header("Objects")]
-    [SerializeField] private KeyBindings keyBindings;
-    [SerializeField] private PlayerSettings playerSettings;
+	[Header("Objects")]
+	[SerializeField] private KeyBindings keyBindings;
+	[SerializeField] private PlayerSettings playerSettings;
     [SerializeField] private PlayerConfig playerConfig;
-    [Header("References")]
-    [SerializeField] private CharacterController characterController;
-    [SerializeField] private Transform groundCheck;
+	
+	[Header("Character stuff")]
+	[SerializeField] private Camera camera;
+	[SerializeField] private CapsuleCollider collider;
+    [SerializeField] private Rigidbody rigidbody;
+	
+	[Header("UI stuff")]
     [SerializeField] private TextMeshProUGUI playerNameplate;
     [SerializeField] private Canvas canvas;
     [SerializeField] private UserInterfaceManager userInterfaceManager;
-    // Privates
-    Vector3 velocity;
-    bool isGrounded;
-    // Private References
-    float groundDistance = 0.05f;
-    public override void OnStartLocalPlayer()
-    {
-        base.OnStartLocalPlayer();
+	
+	[Header("Vars")]
+	// Camera setup
+    private float viewOffset = 0.6f; // The height at which the camera is bound to
+	private float camXRotation = 0.0f;
+    private float xMouseSensitivity = 30.0f;
+    private float yMouseSensitivity = 30.0f;
+
+    [SerializeField] private float gravity = 20.0f;
+    [SerializeField] private float friction = 5f; //Ground friction
+	[SerializeField] private float stopSpeed = 5.0f;
+
+    /* Movement stuff */
+    [SerializeField] private float moveSpeed = 25f;        // Ground move speed
+    [SerializeField] private float runAcceleration = 5; // Ground accel
+    [SerializeField] private float airAcceleration = 2; // Air accel
+    [SerializeField] private float jumpForce = 10.0f;       // The speed at which the character's up axis gains when hitting jump
+	private int jumps = 2;				 // The number of jumps the character has
+	private int maxJumps = 2;    		 // The number of jumps to reset to
+
+	private Vector3 velocity;
+	private bool isGrounded = false;
+    private Vector3 wishMove = Vector3.zero;
+
+	public override void OnStartLocalPlayer()
+	{
+		base.OnStartLocalPlayer();
+		Camera.main.orthographic = false;
+		Camera.main.transform.SetParent(transform);
+		Camera.main.transform.localPosition = rigidbody.position + new Vector3(0f, viewOffset, 0f);
+		Camera.main.transform.localEulerAngles = new Vector3(10f, 0f, 0f);
+		camera = Camera.main;
+
         playerNameplate.text = playerConfig.playerName;
         if (!keyBindings)
             keyBindings = Resources.Load<KeyBindings>("Objects/KeyBindings");
@@ -31,89 +61,148 @@ public class PlayerController : NetworkBehaviour
             playerSettings = Resources.Load<PlayerSettings>("Objects/PlayerSettings");
         if (!playerConfig)
             playerConfig = Resources.Load<PlayerConfig>("Objects/PlayerConfig");
-        if (!characterController)
-            characterController = transform.GetComponent<CharacterController>();
+        if (!rigidbody)
+            rigidbody = transform.GetComponent<Rigidbody>();
         if (!userInterfaceManager)
             userInterfaceManager = GameObject.Find("Canvas").GetComponent<UserInterfaceManager>();
-    }
+		if (!collider)
+			collider = GetComponent<CapsuleCollider>();
+	}
+
+	void OnDisable()
+	{
+		if (isLocalPlayer)
+		{
+            Camera.main.orthographic = true;
+            Camera.main.transform.SetParent(null);
+            Camera.main.transform.localPosition = new Vector3(0f, 70f, 0f);
+            Camera.main.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+		}
+	}
+
     private void Awake()
-    {
-        canvas.worldCamera = Camera.main;
-    }
-    private void OnValidate()
     {
         if (!keyBindings)
             keyBindings = Resources.Load<KeyBindings>("Objects/KeyBindings");
-        if (!playerSettings)
-            playerSettings = Resources.Load<PlayerSettings>("Objects/PlayerSettings");
+        // Hide the cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+		rigidbody.freezeRotation = true;
+    }
+
+	private void OnValidate()
+	{
         if (!playerConfig)
             playerConfig = Resources.Load<PlayerConfig>("Objects/PlayerConfig");
-        if (!characterController)
-            characterController = transform.GetComponent<CharacterController>();
-        if (!groundCheck)
-            Debug.LogWarning($"{gameObject.name} is missing GroundCheck");
-    }
+	}
+
     private void Update()
     {
-        if (!isLocalPlayer) return;
-        MovePlayer();
-        HandleInteraction();
-        if (Input.GetKeyDown(keyBindings.Menu))
-            userInterfaceManager.MenuToggle();
-    }
-    private void HandleInteraction()
-    {
-        if (Input.GetKeyDown(keyBindings.Interact))
-        {
-            var colliders = Physics.OverlapSphere(transform.position, 1);
-            foreach (Collider collider in colliders)
-            {
-                try { collider.gameObject.SendMessage("Interact", gameObject); }
-                catch { }
-            }
+		if (!isLocalPlayer) return;
+
+        /* Ensure that the cursor is locked into the screen */
+        if (Cursor.lockState != CursorLockMode.Locked) {
+            if (Input.GetButtonDown("Fire1"))
+                Cursor.lockState = CursorLockMode.Locked;
         }
+
+        /* Camera rotation stuff, mouse controls this shit */
+        var mouseX = Input.GetAxisRaw("Mouse X") * xMouseSensitivity * Time.deltaTime;
+        var mouseY = Input.GetAxisRaw("Mouse Y") * yMouseSensitivity * Time.deltaTime;
+		
+		camXRotation -= mouseY;
+		camXRotation = Mathf.Clamp(camXRotation, -90f, 90f);
+
+		Camera.main.transform.localRotation = Quaternion.Euler(camXRotation, 0f, 0f);
+		transform.Rotate(Vector3.up * mouseX);
+
+        if(isGrounded)
+            GroundMove();
+        else if(!isGrounded)
+            AirMove();
+		print("Magnitude: " + new Vector2(velocity.x, velocity.z).magnitude);
     }
-    private void MovePlayer()
+
+	private void FixedUpdate()
+	{
+		rigidbody.velocity = velocity;
+	}
+
+	private void OnCollisionEnter()
+	{
+		isGrounded = true;
+		jumps = maxJumps;
+		if (Input.GetButton("Jump"))
+		{
+			velocity.y = jumpForce;
+			jumps = maxJumps - 1;
+		}
+	}
+	
+	private void OnCollisionStay()
+	{
+		isGrounded = true;
+		jumps = maxJumps;
+	}
+	
+	private void OnCollisionExit()
+	{
+		isGrounded = false;
+	}
+
+	private void GroundMove()
+	{
+		float x = 0, z = 0;
+		if (Input.GetKey(keyBindings.Forward))  z += 1;
+		if (Input.GetKey(keyBindings.Backward)) z -= 1;
+		if (Input.GetKey(keyBindings.Right))    x += 1;
+		if (Input.GetKey(keyBindings.Left))     x -= 1;
+		wishMove = x * transform.right + z * transform.forward;
+		if (Input.GetButtonDown("Jump") && jumps > 0)
+		{
+			velocity.y = jumpForce;
+			jumps--;
+		}
+		Friction();
+		Accelerate(runAcceleration);
+	}
+
+	private void AirMove()
+	{
+		if (Input.GetButtonDown("Jump") && jumps > 0)
+		{
+			velocity.y = jumpForce;
+			jumps--;
+		}
+		Accelerate(airAcceleration);
+		velocity.y -= gravity * Time.deltaTime;
+	}
+
+    private void Friction()
     {
-        CheckGround();
-        var inputs = HandleMoveInputs();
-        var move = transform.right * inputs.x + transform.forward * inputs.y;
-        if (Input.GetKey(keyBindings.Sprint))
-            characterController.Move(move * playerSettings.SprintSpeed * Time.deltaTime);
-        else
-            characterController.Move(move * playerSettings.MoveSpeed * Time.deltaTime);
-        HandleJump();
-        HandleGravity();
+		var speed = velocity.magnitude;
+		if (speed > 0)
+		{
+			var control = speed < stopSpeed ? stopSpeed : speed;
+			var drop = control * friction * Time.deltaTime;
+			var newSpeed = speed - drop;
+			if (newSpeed < 0) {
+				newSpeed = 0;
+			}
+			newSpeed /= speed;
+			velocity *= newSpeed;
+		}
     }
-    private void CheckGround()
+
+    private void Accelerate(float accel)
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position - new Vector3(0, groundDistance, 0), groundDistance);
-        if (isGrounded)
-            velocity.y = playerSettings.Gravity;
-    }
-    private Vector2 HandleMoveInputs()
-    {
-        float x = 0f, z = 0f;
-        if (Input.GetKey(keyBindings.Forward))
-            z += 1;
-        if (Input.GetKey(keyBindings.Backward))
-            z -= 1;
-        if (Input.GetKey(keyBindings.Right))
-            x += 1;
-        if (Input.GetKey(keyBindings.Left))
-            x -= 1;
-        return new Vector2(x, z);
-    }
-    private void HandleJump()
-    {
-        if (Input.GetButton("Jump") && (isGrounded || transform.parent))
-            velocity.y = Mathf.Sqrt(3 * -2 * playerSettings.Gravity);
-        else if (transform.parent)
-            velocity.y = 0f;
-    }
-    private void HandleGravity() {
-        if (!transform.parent)
-            velocity.y += playerSettings.Gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
+		var vel = new Vector2(velocity.x, velocity.z);
+		var wishDir = new Vector2(wishMove.x, wishMove.z).normalized;
+		var currentSpeed = Vector2.Dot(vel, wishDir);
+		var addSpeed = moveSpeed - currentSpeed;
+		addSpeed = Mathf.Max(Mathf.Min(addSpeed, accel * Time.deltaTime * moveSpeed), 0);
+		velocity.x += addSpeed * wishDir.x;
+		velocity.z += addSpeed * wishDir.y;
     }
 }
