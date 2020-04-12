848fe26fe3ef6ba5c4c3cedb342e4c995d4cd40f
jwmyr <jwilliammyre@gmail.com>
2020-04-12 16:37:51 -0400

    dance club first commit test

---
+++ GamersGaming/Assets/BallRoation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRoation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Light discoLight = lightGameObject.AddComponent<Light>();
+        discoLight.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, 0, 0.05);
+    }
+}
