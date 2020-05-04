72462fc3481f09225e5d72fe10eca6db440bfee7
Calvin Williams <calvinjwilliams97@gmail.com>
2020-05-04 00:55:41 -0400

    Added dashing back

--- Untitled_FPS/Assets/Scripts/Player/PlayerMovementHandler.cs
+++ Untitled_FPS/Assets/Scripts/Player/PlayerMovementHandler.cs
@@ -7,6 +7,10 @@ public class PlayerMovementHandler : NetworkBehaviour
 {
     [SerializeField] private CharacterController _currentController = null;
     [SerializeField] private float _moveSpeed = 3f;
+    [SerializeField] private float _boostSpeed = 100f;
+    [SerializeField] private float _boostTime = 0.25f;
+    [SerializeField] private float _boostRemaining = 0f;
+    [SerializeField] private bool boosted = false; // if the tagger has boosted in the air already. Reset this on player contact.
     [SerializeField] private float _sprintSpeedMutliplyer = 1.75f;
     [SerializeField] private float _gravity = 9.79f;
     [SerializeField] private float _jumpForce = 4f;
@@ -16,6 +20,7 @@ public class PlayerMovementHandler : NetworkBehaviour
     private Vector2 _rotation = new Vector2();
     private Quaternion _playerTargetRot;
     private Quaternion _cameraTargetRot;
+
     public override void OnStartLocalPlayer()
     {
         base.OnStartLocalPlayer();
@@ -23,13 +28,21 @@ public class PlayerMovementHandler : NetworkBehaviour
         _playerTargetRot = transform.rotation;
         _cameraTargetRot = _player.GetPlayerCamera().transform.rotation;
     }
+
     private void Update()
     {
         if (!base.hasAuthority)
             return;
 
-        if (!Cursor.visible)
+        if (!Cursor.visible && _boostRemaining <= 0)
         {
+            if (Input.GetMouseButtonDown(0) && !boosted)
+            {
+                boosted = true;
+                _boostRemaining = _boostTime;
+                _currentController.Move(Camera.main.transform.forward * Time.deltaTime * _boostSpeed);
+                return;
+            }
             // Player Ground Movement
             var inputMove = Vector3.zero;
             if (Input.GetKey(KeyCode.W))
@@ -57,10 +70,20 @@ public class PlayerMovementHandler : NetworkBehaviour
 
             // Jumping
             if (isGrounded())
+            {
+                boosted = false;
                 m_downVelocity = 0f;
+            }
             if (isGrounded() && Input.GetKey(KeyCode.Space))
                 m_downVelocity = _jumpForce;
         }
+        else
+        {
+            _boostRemaining -= Time.deltaTime;
+            if (_boostRemaining > 0f)
+                _currentController.Move(Camera.main.transform.forward * Time.deltaTime * _boostSpeed);
+        }
+
         m_downVelocity -= _gravity * Time.deltaTime;
         _currentController.Move(new Vector3(0f, m_downVelocity * Time.deltaTime, 0f));
     }
@@ -70,6 +93,7 @@ public class PlayerMovementHandler : NetworkBehaviour
     {
         return Physics.CheckSphere(transform.position - new Vector3(0f, 0.1f, 0f), 0.05f);
     }
+
     private Quaternion LockCameraMovement(Quaternion q)
     {
         q.x /= q.w;
